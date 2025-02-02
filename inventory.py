e7668d7c36129036b66b4dc4c1c175f4b6ef066c
sammy-19 <sammyhills@outlook.com>
2025-02-02 15:24:56 +0200

    Added some changes to the files. Phase one complete

--- inventory.py
+++ inventory.py
@@ -1,9 +1,30 @@
 import sys
+import datetime
 from stock import Stock
-   
+
+# Stocking in of products into the system
+def stocking(s, headings):
+        
+        # creating a temporal list for values of key(product) in a dictionary
+        list = []  
+        prd = input("Product Name: ").strip()
+        qty = int(input("Quantity: "))
+        date = str(datetime.date.today())
+        unit_price = int(input("Unit Price: "))
+        amount = qty*unit_price
+        list += qty, date, unit_price, amount
+        
+        # storing a list as a value of a key (prd) into a dictionary
+        s[prd.capitalize()] = list
+         
 # A dictionary for storing and accessing the items
-storage = {}
-            
+products = {}
+
+#Table headers for distinguishing the columns and proper display of products using PrettyTables
+t_headers = ["Name", "Quantity", "Date Entered", "Unit Price", "Total"]
+error_msg = 2"\nError:Invalid Input! Please Enter appropriate Values\n"
+stock = Stock(products, t_headers) 
+           
 print("Welcome to Inventory Managment App\nWhat do you want to do? follow the prompt below\n")
 
 # A loop that is running the entire program
@@ -14,28 +35,36 @@ while True:
     # Stocking In new items
     if response == "s":
         
-        # Number to determine the number of items a user is entering 
-        num = int(input('\nEnter Number of the types of products to store: '))
+        # Number to determine the number of items a user is entering
+        try:
+            num = int(input('\nEnter Number of the types of products to store: '))
         
         # Entering & Storing the items based on a number specified by user
-        while num > 0:
-            prd, qty = input("Product Name: ").strip(), int(input("Quantity: "))
-            storage[prd] = qty
-            num -= 1
-        stock = Stock(storage)
-
+            while num > 0:
+                stocking(products, t_headers)            
+                num -= 1
+        except ValueError:
+            print(error_msg)
+ 
 # Restocking of items                        
     elif response == "r":
-            print("\n\nRestocking...")
-            prd, qty = input("Product Name: "), int(input("Quantity: "))
-            stock.restocking(prd, qty)
-            print("Success\n")
+            print("\nRestocking...")
+            try:
+                prd, qty= input("Product Name: ").capitalize().strip(), int(input("Quantity: "))            
+            except ValueError:
+                print(error_msg)
+            else:
+                stock.restocking(prd, qty)
+                print("Success\n")
     
  # Outstocking/decreasing the quantity of items
     elif response == 'o':
         print('Outstocking...')
-        prd, qty = input("Product Name: "), int(input("Quantity: "))
-        stock.out_stocking(prd, qty)
+        try:
+            prd, qty = input("Product Name: "), int(input("Quantity: "))
+        except ValueError:
+            print(error_msg)
+            stock.out_stocking(prd, qty)
 
 # Displaying all the stored items together with their quantities                
     elif response == 'v':
--- stock.py
+++ stock.py
@@ -2,30 +2,40 @@ from prettytable import PrettyTable
 
 # For processing data entered by the user
 class Stock:
-    def __init__(self, products):
+    def __init__(self, products, headers):
         self.__products = products
+        self.headers = headers
+        self.__products
 
 # Restocking product quantities
     def restocking(self, key, value):
+        
+        # Searches for a product in a dictionary of products
         if key in self.__products:
-            self.__products[key] += value
+            print(self.__products[key])
+            self.__products[key][0] += value
+            self.__products[key][-1] += (value*self.__products[key][-2])
             return self.__products
+        else:
+            print("No such product")
             
 # To display the data stored in products variable             
     def product_output(self):
-        list = ["S/N", "Name", "Quantity"]
+        #self.headers.before("S/N", 0)
         table = PrettyTable()
-        table.field_names = list
-        p_names, p_qtys = [], []
-        serial = 1
+        table.field_names = self.headers
         
         # Unpacking the products and quantities into separate lists for display in tabular form
         for key, value in self.__products.items():
-            p_names.append(key)
-            p_qtys.append(value)   
-        for p, q in zip(p_names, p_qtys):
-            table.add_row([serial, p, q])
-            serial += 1
+            names, qtys =[], []
+            names.append(key)
+            qtys.append(value)
+            
+            # appends a product name into a temporary list created  in order to add a row in order to add the product and its particulars
+            for p, q in zip(names, qtys):
+                q.insert(0, p)
+                table.add_row(q)
+                q.pop(0)
         return table
 
 # Removing a certain quantity of a product out of the system: Outstocking
@@ -33,9 +43,19 @@ class Stock:
         
         # checking if a product is present then outstock
         if prd in self.__products.keys():
-            self.__products[prd] -= qty
-            print(f'{self.__products[prd]}  Remaining\n')
+            self.__products[prd][0] -= qty
+            #self.__products[prd][-1] -= (qty*+self.__products[prd][-2])
+            print(f'{self.__products[prd][0]}  Remaining\n')
         else:
             print('No such product')
             pass
         return self.__products
+        
+if __name__ == "__main__":
+    dict = {'a':[10, 11, 12], 'b':[1, 2, 3], 'c':[3, 4, 5]}
+    h = [1, 2, 3, 4]
+    stock = Stock(dict, h)
+    #out_stk = stock.out_stocking('a', 5)
+    print(stock.product_output())
+    restock = stock.restocking('a', 6)
+    print(stock.product_output())
